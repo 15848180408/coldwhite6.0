41c73f019deea54cad528092312e6be813893fa5
15848180408 <140040883+15848180408@users.noreply.github.com>
2026-09-03 12:40:25 +0800

    Update CWRootListController.h

--- ColdWhitePrefs/CWRootListController.h
+++ ColdWhitePrefs/CWRootListController.h
@@ -1,3 +1,15 @@
 #import <UIKit/UIKit.h>
-@interface PSListController : UITableViewController { NSMutableArray *_specifiers; } - (id)specifiers; @end
-@interface PSSpecifier : NSObject + (instancetype)preferenceSpecifierNamed:(NSString*)name target:(id)target set:(SEL)set get:(SEL)get detail:(Class)detail cell:(NSInteger)cellType edit:(id)edit; @property(nonatomic,retain) NSMutableDictionary *properties; @end
+
+@interface PSListController : UITableViewController {
+    NSMutableArray *_specifiers;
+}
+- (id)specifiers;
+- (id)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
+@end
+
+@interface PSSpecifier : NSObject
+@property (nonatomic, retain) NSMutableDictionary *properties;
+@end
+
+@interface CWRootListController : PSListController
+@end
