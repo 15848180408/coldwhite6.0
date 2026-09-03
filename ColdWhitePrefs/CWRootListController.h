dd756d1a4bb9a3ea16d8dcf4a066d27aef2b0f11
15848180408 <140040883+15848180408@users.noreply.github.com>
2026-09-03 12:51:25 +0800

    Update CWRootListController.h

--- ColdWhitePrefs/CWRootListController.h
+++ ColdWhitePrefs/CWRootListController.h
@@ -3,12 +3,6 @@
 @interface PSListController : UITableViewController {
     NSMutableArray *_specifiers;
 }
-- (id)specifiers;
-- (id)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
-@end
-
-@interface PSSpecifier : NSObject
-@property (nonatomic, retain) NSMutableDictionary *properties;
 @end
 
 @interface CWRootListController : PSListController
